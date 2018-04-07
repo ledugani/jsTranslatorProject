ecf556854fd41aeee3553e18878b6a5cce545abd
Tom Dugan <thomas@aadwebb.com>
2018-04-07 12:37:26 -0500

    added language arrays

---
+++ main.js
@@ -0,0 +1,62 @@
+var spanish = {
+    "a": "un",
+    "an": "un",
+    "the": "el",
+    "and": "y",
+    "happy": "feliz",
+    "holiday": "fiesta",
+    "holidays": "fiestas",
+    "new": "nuevo",
+    "year": "ano",
+    "christmas": "navidad",
+    "season": "temporada",
+    "seasons": "temporadas",
+    "greeting": "saludo",
+    "greetings": "saludos",
+    "hanukkah": "hanukkah",
+    "warm": "calentar",
+    "wishes": "deseos",
+    "kwanzaa": "kwanzaa"
+};
+
+var german = {
+    "a": "ein",
+    "an": "ein",
+    "the": "das",
+    "and": "und",
+    "happy": "glücklich",
+    "holiday": "urlaub",
+    "holidays": "ferien",
+    "new": "neu",
+    "year": "jahr",
+    "christmas": "weihnachten",
+    "season": "jahreszeit",
+    "seasons": "jahreszeiten",
+    "greeting": "Gruß",
+    "greetings": "Gruße",
+    "hanukkah": "chanukka",
+    "warm": "warm",
+    "wishes": "wunsch",
+    "kwanzaa": "kwanzaa"
+};
+
+var klingon = {
+    "a": ".",
+    "an": "ghaH",
+    "the": ".",
+    "and": "je",
+    "happy": "quch",
+    "holiday": "QI'lop",
+    "holidays": "QI'lop",
+    "new": "chu'",
+    "year": "dis",
+    "christmas": "QISmaS",
+    "season": "maqtagh",
+    "seasons": "maqtagh",
+    "greeting": "ghah 'ej duvan",
+    "greetings": "ghah 'ej duvan",
+    "hanukkah": "hanukkah",
+    "warm": "warm",
+    "wishes": "vIneH",
+    "kwanzaa": "kwanzaa"
+};
